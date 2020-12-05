fbdb51cd952f4e9dc57e841cab972b15877dd95a
jlucasa <40509187+jlucasa@users.noreply.github.com>
2020-12-04 21:03:37 -0700

    Add a bunch of stuff, test out fonts

---
+++ assets/code/header-comment-example.js
@@ -0,0 +1,6 @@
+/**
+  * Authors: <anyone who has contributed/is familiar enough to take ownership of the file>
+  * Date: <the date of this file's creation> <the date of this file's last update>
+  * Description: <a high level description of the file's function>
+  * Assumptions: <any assumptions made toward the successful functioning of the file's code>
+ **/
